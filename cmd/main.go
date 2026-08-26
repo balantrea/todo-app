bd3fee40376f5438e1698458ee21f32bba2955ff
Ryzhkov Leonid <leoneedvip@gmail.com>
2026-08-26 18:53:11 +0300

    adding the repo directory

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/balantrea/todo-app"
 	handler "github.com/balantrea/todo-app/pkg/handler"
+	"github.com/balantrea/todo-app/pkg/repository"
+	service "github.com/balantrea/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	service := service.NewService(repos)
+	handlers := handler.NewHandler(service)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRouters()); err != nil {
 		log.Fatalf("error occurred while running http server: %s\n", err.Error())
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,18 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/balantrea/todo-app/pkg/service"
+	"github.com/gin-gonic/gin"
+)
 
 type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{
+		service: service,
+	}
 }
 
 func (h *Handler) InitRouters() *gin.Engine {
