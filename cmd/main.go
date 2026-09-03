eb651cf6a88b3828d28bfd8996282e4b5bad9dbb
Ryzhkov Leonid <leoneedvip@gmail.com>
2026-09-03 15:51:28 +0300

    adding the SQL tables and congigs

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,29 @@ import (
 	"log"
 
 	"github.com/balantrea/todo-app"
-	handler "github.com/balantrea/todo-app/pkg/handler"
+	"github.com/balantrea/todo-app/pkg/handler"
 	"github.com/balantrea/todo-app/pkg/repository"
-	service "github.com/balantrea/todo-app/pkg/service"
+	"github.com/balantrea/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
-	service := service.NewService(repos)
-	handlers := handler.NewHandler(service)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRouters()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRouters()); err != nil {
 		log.Fatalf("error occurred while running http server: %s\n", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.down.sql
@@ -0,0 +1,11 @@
+DROP TABLE list_item;
+
+DROP TABLE users_lists;
+
+DROP TABLE users;
+
+DROP TABLE todo_lists;
+
+DROP TABLE todo_items;
+
+
